26cf1249a2b5db3128746f6166e491ca79cd5ba5
Samar Choudhary <samchoud@samchoud-OSX.local>
2017-10-09 20:17:18 -0400

    Initial commit

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+oc new-project $1 
+
+
+oc login -u system:admin
+
+oc adm policy add-role-to-user system:image-builder developer 
+oc adm policy add-role-to-user system:registry developer 
+oc adm policy add-role-to-user admin developer 
+oc login -u developer -p developer
+
+# sets the docker env being used and login to minishift registry
+###minishift docker-env
+
+## AS DEV
+oc secrets new docker-pull-secret-dev .dockerconfigjson=${HOME}/.docker/config.json 
+oc secrets link default docker-pull-secret-dev --for=pull 
+
+
+oc create -f blockapps.yaml
+
